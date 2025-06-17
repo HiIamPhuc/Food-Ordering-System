6586d9e075695752cf02a20d8c162152b91a41f3
SalutJeSuisDaniel <ngphuc222@clc.fitus.edu.vn>
2025-06-17 19:54:34 +0700

    Docker deployment for Menu-service

--- backend/menu-service/config/database.js
+++ backend/menu-service/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
-      process.env.MONGODB_URI || 'mongodb://localhost:27017/food_ordering_menu',
+      process.env.MONGODB_URI || 'mongodb://localhost:27018/food_ordering_menu',
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
